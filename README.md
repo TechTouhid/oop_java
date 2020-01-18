cae13f82470e0c5ec9465e566b077d2e2a5d6a55
Touhidalamin <touhidalamin123@gmail.com>
2020-01-18 23:32:38 +0800

    Done up to Data type

---
+++ README.md
@@ -0,0 +1,9 @@
+# oop_java
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Touhidalamin/oop_java.git
+git push -u origin master
+
+git remote add origin https://github.com/Touhidalamin/oop_java.git
+git push -u origin master
--- src/Hello.java
+++ src/Hello.java
@@ -6,5 +6,35 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+
+        int myfirstvar = 50;
+        System.out.println(myfirstvar);
+
+        byte mybyte = 10;
+        short myshort = 20;
+        long mylong = 5000L;
+        System.out.println(mylong + 10*(mybyte + myshort + myfirstvar));
+
+        // the numbers can be separate using underscore
+        // for float need to use f for double need to use d
+        float myfolat =5.488_554_546_48f;
+        double mydouble = 5.44d;
+        boolean mybool = true;
+
+        // write sout and press enter to have System.out.println()
+        System.out.println(myfolat);
+        System.out.println(mydouble);
+
+        // defining the character type data
+
+        char mychar = 'A';
+        char unichar = '\u00A9';
+        System.out.println("Unicode character " + unichar);
+
+
+
+
+
+
     }
 }
