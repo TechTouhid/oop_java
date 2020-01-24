ead7bc52bd04f9afdd84c37f0896bb1439162888
Touhidur Rahman <touhidalamin123@gmail.com>
2020-01-24 19:11:39 +0800

    Done up to Section 5: Parsing string to other value like int float double etc

---
+++ src/Parsing_Str_to_other_value.java
@@ -0,0 +1,16 @@
+public class Parsing_Str_to_other_value {
+    public static void main(String[] args) {
+        String numberAsString = "2020";
+        System.out.println("Number as string = " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("Number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("Number as string = " + numberAsString);
+        System.out.println("Number = " + number);
+
+    }
+}
