496c7156998616c634aec489245c04c4ab45795a
Touhidur Rahman <touhidalamin123@gmail.com>
2020-07-21 20:15:51 +0600

    did the database work

--- src/com/touhid/TimDatabase.java
+++ src/com/touhid/TimDatabase.java
@@ -1,9 +1,9 @@
+// need to change the time zone if needed and it need to run in the database as a query
+// SET GLOBAL time_zone = '+6:00';
+
 package com.touhid;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TimDatabase {
     public static void main(String[] args) {
@@ -14,9 +14,35 @@ public class TimDatabase {
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "");
+//            conn.setAutoCommit(false); // we can set the auto commit false to stop saving the changes in database
             Statement statement = conn.createStatement();
+
+            // Creating the table in student database name contacts
             statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
                                   " (name varchar(20), phone INTEGER(20), email varchar(20) )");
+
+            // Inserting data into the contacts table
+//            statement.execute("INSERT INTO contacts(name, phone, email)" +
+//                                  "VALUE ('Touhid', 180503104, 'Touhid@gmail.com')");
+
+//            statement.execute("UPDATE prfile2 SET 'last name' = x 'first name' = ? , address= ? , city= ? , state= ? , telephone= ?  WHERE id = ?");
+
+
+            // Updating the phone number
+//            statement.execute("UPDATE contacts SET phone=01748753174 WHERE name='Touhid' ");
+
+            // Deleting the a row
+//            statement.execute("DELETE FROM contacts WHERE name='Touhid'");
+
+            // Getting the data form the database
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()) {
+                System.out.println(results.getString("name") + " " +
+                                   results.getInt("phone") + " " +
+                                   results.getString("email"));
+            }
+            results.close();
 
             // Closing the connections and the statements
             // Important note is that statements need to close first
